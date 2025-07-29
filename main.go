c59fc6c917d4d513072dbd3d443819d186ac0185
alexey-y-a <lexus2157@mail.ru>
2025-07-29 22:45:30 +0300

    fix1

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	const (
 		USD_TO_EUR = 0.85
@@ -8,3 +10,11 @@ func main() {
 
 	const EUR_TO_RUB = USD_TO_RUB / USD_TO_EUR
 }
+
+func getUserInput() int {
+	var num int
+	fmt.Scan(&num)
+	return num
+}
+
+func calculate(num1, num2, num3 float64) {}
